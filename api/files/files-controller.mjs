32271f3d0977996d3a3dc28a459ab12e2ab47f75
Rodrigo Colín <rodrigo.cln@ciencias.unam.mx>
2022-07-06 17:17:39 -0500

    subida de archivos de video y texto

--- api/files/files-controller.mjs
+++ api/files/files-controller.mjs
@@ -1,8 +1,64 @@
+import multer from 'multer' // middleware para subir archivos
+
+/**
+ * Sube al servidor un archivo de video
+ * @param {Object} req - Petición (request) recibida por http
+ * @param {Object} res - Respuesta (response) a enviar por http
+ */
+ export function uploadVideo(req, res){
+    // 'video' debe coincider con el nombre en formData.append('video', videoFile) desde formulario front-end
+    const upload = multer({ dest: 'uploads/'}).single('video');
+
+    upload(req, res, function(err){
+        if (err instanceof multer.MulterError){ // error de Multer
+            return res.status(400).json({ message: 'No se pudo procesar el archivo enviado'});
+        }
+        if (err){ // cualquier otro error
+            return res.status(500).json({ message: 'Error al subir el archivo'});
+        }
+        // código cuando no hay error:
+        res.status(200).json({ file: req.file, message: 'Video subido correctamente'});
+    });
+}
+
 /**
  * Sube al servidor un archivo de imagen
  * @param {Object} req - Petición (request) recibida por http
  * @param {Object} res - Respuesta (response) a enviar por http
  */
 export function uploadImage(req, res){
-    res.status(200).json({ file: req.file, message: 'Image uploaded successfully'});
+    // 'image' debe coincider con el nombre en formData.append('image', imageFile) desde formulario front-end
+    const upload = multer({ dest: 'uploads/'}).single('image');
+
+    upload(req, res, function(err){
+        if (err instanceof multer.MulterError){ // error de Multer
+            return res.status(400).json({ message: 'No se pudo procesar el archivo enviado'});
+        }
+        if (err){ // cualquier otro error
+            return res.status(500).json({ message: 'Error al subir el archivo'});
+        }
+        // código cuando no hay error:
+        res.status(200).json({ file: req.file, message: 'Imagen subida correctamente'});
+    });
+}
+
+/**
+ * Sube al servidor un documento de texto (pdf)
+ * @param {Object} req - Petición (request) recibida por http
+ * @param {Object} res - Respuesta (response) a enviar por http
+ */
+ export function uploadDocument(req, res){
+    // 'document' debe coincider con el nombre en formData.append('document', documentFile) desde formulario front-end
+    const upload = multer({ dest: 'uploads/'}).single('document');
+
+    upload(req, res, function(err){
+        if (err instanceof multer.MulterError){ // error de Multer
+            return res.status(400).json({ message: 'No se pudo procesar el archivo enviado'});
+        }
+        if (err){ // cualquier otro error
+            return res.status(500).json({ message: 'Error al subir el archivo'});
+        }
+        // código cuando no hay error:
+        res.status(200).json({ file: req.file, message: 'Documento subido correctamente'});
+    });
 }
--- api/files/files-routes.mjs
+++ api/files/files-routes.mjs
@@ -1,13 +1,10 @@
 import express from 'express';
 const router = express.Router();
 import * as controller from './files-controller.mjs';
-import multer from 'multer' // middleware para subir archivos
+import * as auth from '../../services/auth-service.mjs';
 
-const upload = multer({ dest: 'uploads/'}) // ruta de los archivos
-
-// TODO: Middleware require también verificación (auth.requireLogin)
-
-// 'file' debe coincider con el nombre en formData.append('file', fileImage) desde formulario front-end
-router.post('/upload/image', upload.single('file'), controller.uploadImage);
+router.post('/file/video', auth.requireLogin, controller.uploadVideo);
+router.post('/file/image', auth.requireLogin, controller.uploadImage);
+router.post('/file/document', auth.requireLogin, controller.uploadDocument);
 
 export default router;
